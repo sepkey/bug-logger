d876cea659619aa0027267901dbcc1e5ebdf0ce9
Sepide Kia <sepidekia6990@gmail.com>
2024-05-28 08:27:46 +0330

    feat: add list of bugs

--- app/bugs/page.tsx
+++ app/bugs/page.tsx
@@ -1,13 +1,44 @@
-import { Button } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 
-export default function Bugs() {
+export default async function Bugs() {
+  const bugs = await prisma.bug.findMany();
+
   return (
     <div>
-      Users page
-      <Button>
-        <Link href="/bugs/new">New Bug</Link>
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/bugs/new">New Bug</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.Cell>Bug</Table.Cell>
+            <Table.Cell className="hidden md:table-cell">Status</Table.Cell>
+            <Table.Cell className="hidden md:table-cell">Created at</Table.Cell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {bugs.map((bug) => {
+            return (
+              <Table.Row key={bug.id}>
+                <Table.Cell>
+                  {bug.title}
+                  <div className="block md:hidden">{bug.status}</div>
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  {bug.title}
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  {bug.createdAt.toDateString()}
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <Theme accentColor="orange">
           <Navbar />
           <main className="p-5">{children}</main>
-          <ThemePanel />
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
