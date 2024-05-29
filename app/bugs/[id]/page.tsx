3e03a9d7e2ba96feda8d8761ee6a2081509f0dc6
Sepide Kia <sepidekia6990@gmail.com>
2024-05-29 09:40:40 +0330

    feat: add react-markdown lib

--- app/bugs/[id]/page.tsx
+++ app/bugs/[id]/page.tsx
@@ -2,6 +2,7 @@ import StatusBadge from '@/app/_components/StatusBadge';
 import prisma from '@/prisma/client';
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
+import ReactMarkdown from 'react-markdown';
 
 type Props = {
   params: { id: string };
@@ -24,7 +25,9 @@ export default async function BugDetail({ params }: Props) {
         <StatusBadge status={bug.status} />
         <Text>{bug.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>{bug.description}</Card>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{bug.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 }
