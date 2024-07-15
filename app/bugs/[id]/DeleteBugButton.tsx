2183c0aa174af0097fdcb7b581b5605cbe1987d4
Sepide Kia <sepidekia6990@gmail.com>
2024-07-15 10:10:01 +0330

    ft: add confirmation dialog to the delete

--- app/bugs/[id]/DeleteBugButton.tsx
+++ app/bugs/[id]/DeleteBugButton.tsx
@@ -1,12 +1,34 @@
-import { Pencil2Icon, TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
-import Link from 'next/link';
+import { TrashIcon } from '@radix-ui/react-icons';
+import { Button, Dialog, Flex } from '@radix-ui/themes';
 
 export default function DeleteBugButton({ bugId }: { bugId: number }) {
   return (
-    <Button color="red">
-      <TrashIcon />
-      Delete bug
-    </Button>
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete bug
+        </Button>
+      </Dialog.Trigger>
+
+      <Dialog.Content maxWidth="450px">
+        <Dialog.Title>Confirm deletion</Dialog.Title>
+        <Dialog.Description size="2" mb="4">
+          Are you sure you want to delete this bug? this action can not be
+          undone.
+        </Dialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <Dialog.Close>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </Dialog.Close>
+          <Dialog.Close>
+            <Button color="red">Delete bug</Button>
+          </Dialog.Close>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 }
